5a7614e31888c6acd6ee8f2a363b3937127ba324
RAFLI <rafli1162003@gmail.com>
2021-07-18 13:02:10 +0700

    add some section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        // map: 'url("/dist/assets/map.svg")',
+        'map': "url('assets/map.svg')",
+      }),
+      colors: (theme) => ({
+        'blue-dark': '#115B76',
+        'green-dark': '#1DC0AD',
+        'green-light': '#28dac5',
+      }),
+      height: (theme) => ({
+        'footer-bg': '440px',
+        hero: '400px',
+        'hero-mobile': '280px',
+        contactUs: '320px',
+      }),
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+        montserrat: '"Montserrat Alternates", sans-serif',
+        'open-sans': '"Open Sans", sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
